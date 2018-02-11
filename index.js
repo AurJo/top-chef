4b5e6b3038f39d5c88699eb724f9f4b6c5254b54
AurelieJolas <aurelie.jolas@devinci.fr>
2018-02-11 16:36:57 +0100

    récupération des données michelin et écriture sur le fichier json

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var request = require("request");
 var cheerio = require("cheerio");
+var fs = require("fs");
+
+
 var lien = "https://restaurant.michelin.fr";
 var url = "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin";
 
-var listAdresse = [];
+var pathJsonFile = "package.json";
+var textJson = '{ "MichelinRestaurants" : [';
+
+fs.writeFileSync(pathJsonFile, textJson);
+
+
 request(
   {
-
   uri: url,
   },function(error, response, body) {
     var $ = cheerio.load(body);
 
     var nbPage = $(".mr-pager-item")[12].children[0].attribs["attr-page-number"];
-    console.log(nbPage);
 
     for (var i=1; i<=nbPage; i++)
     {
@@ -26,41 +32,51 @@ request(
         {
           var link = $(this);
           var href = link.attr("href");
-          var titre = link.find(".poi_card-display-title").text();
           var adresse = lien+href;
-          listAdresse.push(adresse);
-          //console.log(titre + "->  " + adresse);
 
           request(
           {
             uri: adresse,
           },function(error, response, body){
-            var $2 = cheerio.load(body);
 
-            console.log(titre);
-
-            var nbEtoile = $2(".content-wrapper").text()
-            console.log(nbEtoile);
+            var name;
+            var lieu;
+            var codeP;
+            var ville;
+            var tel;
 
             try{
-              var lieu = $2(".thoroughfare")[0].children[0].data;
-              console.log(lieu);
+
+              var $2 = cheerio.load(body);
+
+              name = $2(".poi_intro-display-title").text().replace("\n", "");
+
+              try{
+                lieu = $2(".thoroughfare")[0].children[0].data;
+              }
+              catch(e){
+                console.log(e);
+              }
+
+              try{
+                codeP = $2(".postal-code")[0].children[0].data;
+              }
+              catch(e){
+                console.log(e);
+              }
+
+              ville = $2(".locality")[0].children[0].data;
+
+              tel = $2(".tel").text();
+
+              var restaurantLineJson ='\n{ "name": "'+name +'", "adresse" : { "quartier" : "' + lieu + '", "codePostal" : "' + codeP + '", "ville" : "' + ville +'" }, "telephone" : "' + tel + '" },';
+              console.log(restaurantLineJson);
+              fs.appendFileSync(pathJsonFile, restaurantLineJson);
+
             }
             catch(e){
               console.log(e);
             }
-
-            var codeP = $2(".postal-code")[0].children[0].data;
-            console.log(codeP);
-
-            var ville = $2(".locality")[0].children[0].data;
-            console.log(ville);
-
-            var specialite = $2(".poi_intro-display-cuisines").text()
-            console.log(specialite);
-
-            var tel = $2(".tel").text();
-            console.log(tel);
           });
         });
       });
